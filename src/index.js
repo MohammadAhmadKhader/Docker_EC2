09f0fe8f9b5bc29404628dd8c44b18df351e0af8
Mohammad Khader <MohammadKhaderr1999@gmail.com>
2024-05-27 23:13:35 +0300

    changing response

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ app.listen(PORT,()=>{
 app.get("/",async(req,res)=>{
     redisClient.set('products',"products....");
 
-    res.status(200).json({message:"products set successfully"})
+    res.status(200).json({message:"products set successfully",usingDockerHubImage:true})
 })
 
 app.get("/data",async(req,res)=>{
